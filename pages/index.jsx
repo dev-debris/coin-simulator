c934819eba8be26dec0cecbfc7db82e444b523e3
안호진 <asdv94@naver.com>
2023-02-15 18:05:01 +0900

    feat: migrate files to Next.js structure

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Container from '../src/components/Container';
+import Resume from '../src/components/Resume';
+
+function HomePage() {
+  return (
+    <Container>
+      <Resume />
+    </Container>
+  );
+}
+
+export default HomePage;
