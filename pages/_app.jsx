db53f5b30793fbc1d9be3c3311870ba85284141b
안호진 <47904304+HojinAn@users.noreply.github.com>
2023-02-23 09:00:33 +0900

    feat: 전역 라이브러리 추가 (#17)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,22 @@
-import {Fragment} from 'react';
+import {Hydrate, QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+import {useState} from 'react';
+import {RecoilRoot} from 'recoil';
 import GlobalStyle from '../src/styles/global';
 
 function MyApp({Component, pageProps}) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <Fragment>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </Fragment>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
 
