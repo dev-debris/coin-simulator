656e33a898406f3910bf0cfe4b2f32d3dbb9dc35
ahntree <akh9804@gmail.com>
2023-03-05 00:05:07 +0900

    feat: 포트폴리오 컴포넌트 마크업 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,29 @@ function HomePage() {
       <Root>
         <Left>
           <LeftChild>
-            <Portfolio />
+            {/**
+             * @todo 실제 값으로 대체하기
+             */}
+            <Portfolio
+              netAssets={123456789}
+              remainingCash={10000000000}
+              coinList={[
+                {
+                  name: '비트코인',
+                  subName: 'BTC',
+                  averagePrice: 29773000,
+                  currentPrice: 30000000,
+                  quantity: 100,
+                },
+                {
+                  name: '이더리움',
+                  subName: 'ETH',
+                  averagePrice: 30000000,
+                  currentPrice: 29773000,
+                  quantity: 1200,
+                },
+              ]}
+            />
             <Trade />
           </LeftChild>
           <LeftChild>
