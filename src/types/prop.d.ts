3274188662110398a06f7780832eb77d151ca6bd
akh9804 <akh9804@gmail.com>
2023-03-06 20:39:26 +0900

    feat: 포트폴리오 컴포넌트 마크업 추가 (#21)

--- src/types/prop.d.ts
+++ src/types/prop.d.ts
@@ -1,3 +1,15 @@
 interface ChildrenProp {
   children: import('react').ReactNode;
 }
+
+interface PortfolioProp {
+  netAssets: number;
+  remainingCash: number;
+  coinList: Array<{
+    name: string;
+    subName: string;
+    averagePrice: number;
+    currentPrice: number;
+    quantity: number;
+  }>;
+}
