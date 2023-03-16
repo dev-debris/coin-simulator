ff1aec4df29fc2f5fcc3794baf7139e614b06375
Hojin An <devian3143@gmail.com>
2023-03-16 23:25:19 +0900

    refactor: 변환한 ticker 코드 리팩토링

--- src/hooks/useStockDetail.ts
+++ src/hooks/useStockDetail.ts
@@ -7,10 +7,14 @@ const useStockDetail = (ticker: TickerResponse) => {
 
   useEffect(() => {
     const converToKrw = (no: number) => new Intl.NumberFormat('kr', {style: 'currency', currency: 'KRW'}).format(no);
-    const tickerAfter = {} as TickerResponse;
-    for (const [k, v] of Object.entries(ticker)) {
-      tickerAfter[k] = k.includes('price') ? converToKrw(v) : v;
-    }
+
+    const tickerAfter = Object.entries(ticker).reduce(
+      (ticker, [k, v]) => ({
+        ...ticker,
+        [k]: k.includes('price') ? converToKrw(v) : v,
+      }),
+      {} as TickerResponse
+    );
     setConvertedTicker(tickerAfter);
   }, [ticker]);
 
