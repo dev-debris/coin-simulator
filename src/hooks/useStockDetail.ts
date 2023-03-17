6db7e8706f5a2ae1903816fee345030fa73c9260
Hojin An <devian3143@gmail.com>
2023-03-17 10:49:55 +0900

    refactor: ticker 변환에 사용된 불필요한 useEffect 제거

--- src/hooks/useStockDetail.ts
+++ src/hooks/useStockDetail.ts
@@ -1,22 +1,17 @@
-import {useEffect, useState} from 'react';
 import useCandleData from './useCandleData';
 
 const useStockDetail = (ticker: TickerResponse) => {
-  const [convertedTicker, setConvertedTicker] = useState(ticker);
   const data = useCandleData();
 
-  useEffect(() => {
-    const converToKrw = (no: number) => new Intl.NumberFormat('kr', {style: 'currency', currency: 'KRW'}).format(no);
+  const converToKrw = (no: number) => new Intl.NumberFormat('kr', {style: 'currency', currency: 'KRW'}).format(no);
 
-    const tickerAfter = Object.entries(ticker).reduce(
-      (ticker, [k, v]) => ({
-        ...ticker,
-        [k]: k.includes('price') ? converToKrw(v) : v,
-      }),
-      {} as TickerResponse
-    );
-    setConvertedTicker(tickerAfter);
-  }, [ticker]);
+  const convertedTicker = Object.entries(ticker).reduce(
+    (ticker, [k, v]) => ({
+      ...ticker,
+      [k]: k.includes('price') ? converToKrw(v) : v,
+    }),
+    {} as TickerResponse
+  );
 
   return {data, convertedTicker};
 };
