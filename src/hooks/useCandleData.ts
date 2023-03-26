b243f622efd9394c6aff8e82edc31d6981b3a8e9
ahntree <akh9804@gmail.com>
2023-03-26 21:13:22 +0900

    useCandleData 임시 기본 쿼리 변수로 두고 재사용

--- src/hooks/useCandleData.ts
+++ src/hooks/useCandleData.ts
@@ -4,38 +4,19 @@ import {candleState} from '@/atoms';
 import {MARKET_CODE} from '@/constants';
 import {useDayCandleQuery, useMinuteCandleQuery, useMonthCandleQuery, useWeekCandleQuery} from './queries';
 
+/**
+ * @todo 상태 변경 가능하도록 수정하기
+ */
+const DEFAULT_QUERIES = {count: 20, market: MARKET_CODE['krw-btc']};
+
 const useCandleData = () => {
   const [candleType] = useRecoilState(candleState);
   const [data, setData] = useState<Candle[]>([]);
 
-  const {refetch: refetchMinute} = useMinuteCandleQuery({
-    paths: [15],
-    queries: {
-      count: 20,
-      market: MARKET_CODE['krw-btc'],
-    },
-  });
-
-  const {refetch: refetchDay} = useDayCandleQuery({
-    queries: {
-      count: 20,
-      market: MARKET_CODE['krw-btc'],
-    },
-  });
-
-  const {refetch: refetchWeek} = useWeekCandleQuery({
-    queries: {
-      count: 20,
-      market: MARKET_CODE['krw-btc'],
-    },
-  });
-
-  const {refetch: refetchMonth} = useMonthCandleQuery({
-    queries: {
-      count: 20,
-      market: MARKET_CODE['krw-btc'],
-    },
-  });
+  const {refetch: refetchMinute} = useMinuteCandleQuery({paths: [15], queries: DEFAULT_QUERIES});
+  const {refetch: refetchDay} = useDayCandleQuery({queries: DEFAULT_QUERIES});
+  const {refetch: refetchWeek} = useWeekCandleQuery({queries: DEFAULT_QUERIES});
+  const {refetch: refetchMonth} = useMonthCandleQuery({queries: DEFAULT_QUERIES});
 
   useEffect(() => {
     const refetchData = () => {
