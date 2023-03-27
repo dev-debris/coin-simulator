83b4a343dfd884c9d2fec4d27a6273bd186f8915
Hojin An <devian3143@gmail.com>
2023-03-27 18:10:26 +0900

    refactor: stockdetail 폴더 삭제

--- src/components/stockdetail/StockDetailChart.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const ApexChart = dynamic(() => import('react-apexcharts').then(ReactApexChart => ReactApexChart), {ssr: false});
-
-const StockDetailChart = (props: ApexChartProp) => {
-  return <ApexChart {...props} />;
-};
-
-export default StockDetailChart;
