f85c41f205296bbc15a45a86c90d5c49f79f124e
ahntree <akh9804@gmail.com>
2023-04-15 01:48:22 +0900

    fix: 제거된 prop로 인한 빌드 오류 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,29 +14,7 @@ function HomePage() {
       <Root>
         <Left>
           <LeftChild>
-            {/**
-             * @todo 실제 값으로 대체하기
-             */}
-            <Portfolio
-              netAssets={123456789}
-              remainingCash={10000000000}
-              coinList={[
-                {
-                  name: '비트코인',
-                  subName: 'BTC',
-                  averagePrice: 29773000,
-                  currentPrice: 30000000,
-                  quantity: 100,
-                },
-                {
-                  name: '이더리움',
-                  subName: 'ETH',
-                  averagePrice: 30000000,
-                  currentPrice: 29773000,
-                  quantity: 1200,
-                },
-              ]}
-            />
+            <Portfolio />
             <Trade />
           </LeftChild>
           <LeftChild>
