6a58f97e458d0b338bd28f91f097b6d3eb0ad9c0
ahntree <akh9804@gmail.com>
2023-05-13 21:31:47 +0900

    feat: 매수, 매도 기능 추가

--- src/components/Trade/Trade.tsx
+++ src/components/Trade/Trade.tsx
@@ -1,51 +1,23 @@
-import {ChangeEventHandler, useState} from 'react';
 import {useRecoilValue} from 'recoil';
 import {selectedCoinState} from '@/recoil/atoms';
+import TradeCoin from '../TradeCoin/TradeCoin';
+import TradeOptions from '../TradeOptions/TradeOptions';
 import * as S from './Trade.style';
 
 function Trade() {
   const selectedCoin = useRecoilValue(selectedCoinState);
-  const [quantity, setQuantity] = useState('0');
-  const totalPrice = Number(quantity.replace(/,/g, '')) * 10000;
-
-  const onChangeQuantity: ChangeEventHandler<HTMLInputElement> = e => {
-    const newQuantity = e.target.value.replace(/\D/g, '');
-    setQuantity(newQuantity.length ? Number(newQuantity).toLocaleString() : '0');
-  };
 
   return (
     <S.Wrapper>
       <S.Title>매수 / 매도</S.Title>
 
-      <S.SelectedCoinSection>
-        {selectedCoin ? (
-          <>
-            <S.SelectedCoinName>{selectedCoin.korean_name}</S.SelectedCoinName>
-            <S.SelectedCoinPrice>10000</S.SelectedCoinPrice>
-          </>
-        ) : (
-          <S.NoSelectedCoin>매수/매도할 코인을 선택해 주세요</S.NoSelectedCoin>
-        )}
-      </S.SelectedCoinSection>
-
-      {selectedCoin && (
-        <S.TradeSection>
-          <S.TradeBlock>
-            <S.TradeTitle>거래 종류</S.TradeTitle>
-            <S.TradeAction>
-              <S.TradeActionOption>매수</S.TradeActionOption>
-              <S.TradeActionOption>매도</S.TradeActionOption>
-            </S.TradeAction>
-          </S.TradeBlock>
-          <S.TradeBlock>
-            <S.TradeTitle>수량</S.TradeTitle>
-            <S.TradeQuantity type="text" value={quantity} onChange={onChangeQuantity} />
-          </S.TradeBlock>
-          <S.TradeBlock>
-            <S.TradeTitle>총 가격</S.TradeTitle>
-            <S.TradePrice>{totalPrice.toLocaleString()} KRW</S.TradePrice>
-          </S.TradeBlock>
-        </S.TradeSection>
+      {selectedCoin ? (
+        <>
+          <TradeCoin selectedCoin={selectedCoin} />
+          <TradeOptions selectedCoin={selectedCoin} />
+        </>
+      ) : (
+        <S.NoCoin>매수/매도할 코인을 선택해 주세요</S.NoCoin>
       )}
     </S.Wrapper>
   );
---
+++ src/components/TradeOptions/TradeOptions.style.ts
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+import NumberInput from '../NumberInput/NumberInput';
+
+export const Wrapper = styled.section``;
+
+export const OptionBlock = styled.div``;
+
+export const OptionTitle = styled.dt``;
+
+export const TradeActionSelect = styled.select``;
+
+export const TradeActionOption = styled.option``;
+
+export const TradeQuantity = styled(NumberInput)``;
+
+export const TotalPrice = styled.span``;
+
+export const SubmitButton = styled.button``;
